bf997ef027c399198dbcbbfb700d4c94c7450375
intxff <intxff@gmail.com>
2023-11-10 21:20:36 +0800

    feat: use bqf to enhance quickfix

---
+++ config/nvim/lua/plugins/bqf.lua
@@ -0,0 +1,13 @@
+require("bqf").setup({
+	auto_enable = true,
+	magic_window = true,
+	auto_resize_height = true,
+	func_map = {
+		pscrolldown = "<C-d>",
+		pscrollup = "<C-u>",
+	},
+	preview = {
+		auto_preview = true,
+	},
+	border = "rounded",
+})
